2cfc5521dd70cf14e5c9b9f0a10b4d526e9746d8
Dgeph <jeffduffu15@gmail.com>
2025-12-09 02:59:34 +0000

    Add files via upload

---
+++ multiplication_table.py
@@ -0,0 +1,12 @@
+number = int(input("Enter your number: "))
+
+multiplication_factor = 1
+
+while multiplication_factor <= 20:
+    answer = multiplication_factor * number
+    print(f"{multiplication_factor} x {number} = {answer}")
+    multiplication_factor += 1
+
+
+
+
